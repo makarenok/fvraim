5c072564fae6b35af975176649cc8e9d4ce406da
Makarenok <makarenok.roman@gmail.com>
2014-03-10 16:27:38 +0800

    up

--- protected/views/site/_view.php
+++ protected/views/site/_view.php
@@ -1,5 +1,5 @@
 <div class="itemsevents grid-sizer-event <?php echo $data['sort']; ?>">
-<div class="item-overlay" style="top: 0px;background-image:url('<?php echo Yii::app()->baseUrl.$data["link"]; ?>')"></div>
+<div class="item-overlay" style="background-image:url('<?php echo Yii::app()->baseUrl.$data["link"]; ?>')"></div>
 <div class="item-outer-details">
 <a title="<?php echo $data["title"]; ?>" href="">
 <h1><?php echo $data["title"]; ?></h1> </a>
